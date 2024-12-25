b40f0fa3247744ac911a8fd8ed847d1bf13c1f9d
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-12-25 11:53:00 +0530

    Netflix Extension

---
+++ Netflix/ToolWindows/MyToolWindow.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Imaging;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Netflix
+{
+    public class MyToolWindow : BaseToolWindow<MyToolWindow>
+    {
+        public override string GetTitle(int toolWindowId) => "Netflix";
+
+        public override Type PaneType => typeof(Pane);
+
+        public override Task<FrameworkElement> CreateAsync(int toolWindowId, CancellationToken cancellationToken)
+        {
+            return Task.FromResult<FrameworkElement>(new MyToolWindowControl());
+        }
+
+        [Guid("ba1bcb07-fbec-4840-a40c-a88bc4cb0c09")]
+        internal class Pane : ToolkitToolWindowPane
+        {
+            public Pane()
+            {
+                BitmapImageMoniker = KnownMonikers.ToolWindow;
+            }
+        }
+    }
+}
---
+++ Netflix/ToolWindows/MyToolWindowControl.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Netflix
+{
+    public partial class MyToolWindowControl : UserControl
+    {
+        public MyToolWindowControl()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+        private async void InitializeAsync()
+        {
+            Random rnd = new Random();
+            string subFolder = rnd.Next().ToString();
+            var env = await Microsoft.Web.WebView2.Core.CoreWebView2Environment.CreateAsync(userDataFolder: Path.Combine(Path.GetTempPath(), $"{Environment.UserName}", subFolder));
+            await Netflix.EnsureCoreWebView2Async(env);
+            var request = Netflix.CoreWebView2.Environment.CreateWebResourceRequest("https://www.netflix.com/in/", "GET", null, null);
+            Netflix.CoreWebView2.NavigateWithWebResourceRequest(request);
+        }
+    }
+}
